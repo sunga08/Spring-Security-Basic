f86c54e312b51ab8a2d617dad180a7eaccb3ff7d
sunga <eert2260@naver.com>
2023-05-07 16:35:37 +0900

    시큐리티 설정

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록 됨
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll() /**antMatchers에 설정된 패턴 이외 url은 인증 없이 접속 가능**/
+                /**권한이 없는 페이지로 요청이 들어올 때 로그인 페이지로 이동**/
+                .and()
+                .formLogin()
+                .loginPage("/login");
+    }
+}
--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //View를 리턴
 public class IndexController {
@@ -12,8 +13,38 @@ public class IndexController {
         return "index"; //자동으로 src/main/resources/templates/index.mustache 로 잡힘
     }
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "test";
+    @GetMapping("/user")
+    @ResponseBody
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    @ResponseBody
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login") //스프링 시큐리티가 낚아채버림
+    public String login(){
+        return "loginForm";
+    }
+
+    @GetMapping("/join")
+    @ResponseBody
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    @ResponseBody
+    public String joinProc(){
+        return "회원가입 완료";
     }
 }
